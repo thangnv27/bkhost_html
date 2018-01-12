172e8c55c5e6f1c198ab2507616ee2510bee523f
dungvuit <dungvuit@gmail.com>
2018-01-12 20:51:43 +0700

    File Bao Mat SSL

---
+++ bao-mat-ssl.php
@@ -0,0 +1,137 @@
+<?php include("header.php"); ?>
+<div class="layer-old-1">
+  <div class="slide-bao-mat-ssl text-center">
+      <div class="container">
+          <p class="text-slide-1">HIỂU HƠN VỀ BẢO MẬT SSL</p>
+          <p class="text-slide-2">Bảo mật chứng chỉ SSL là gì?</p>
+      </div>
+  </div>
+  <div class="text-slide-3">
+      <div class="container">
+          <p>
+            SSL đảm bảo rằng tất cả các dữ liệu được truyền giữa các máy chủ web và các trình duyệt được mang tính riêng tư.
+          </p>
+      </div>
+  </div>
+  <div class="content-1">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6">
+        </div>
+        <div class="col-md-6">
+          <p class="text-1">SSL là gì?</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 text-right">
+          <p class="text-1">
+            SSL làm việc như thế nào
+          </p>
+          <div class="row">
+            <div class="col-md-11 col-md-offset-1">
+              <p class="text-2">
+                Công nghệ SSL là bảo vệ những giao dịch trực tuyến và giúp nâng cao mức độ tin cậy của Website đối với khách hàng chỉ trong 3 bước
+              </p>
+            </div>
+          </div>
+          <div class="row text-center">
+            <div class="col-md-3"></div>
+            <div class="col-md-3">
+              <img src="images/icon-ssl-128.png">
+            </div>
+            <div class="col-md-3">
+              <img src="images/icon-website.png">
+            </div>
+            <div class="col-md-3">
+              <img src="images/icon-trust.png">
+            </div>
+          </div>
+          <div class="row text-center description-ssl">
+            <div class="col-md-3"></div>
+            <div class="col-md-3">
+              <p class="text-5">
+                SSL mã hóa các thông tin nhạy cảm trong quá trình giao dịch trực tuyến
+              </p>
+            </div>
+            <div class="col-md-3">
+              <p class="text-5">
+                Mỗi chứng chỉ SSL được tạo ra cho một Website duy nhất
+              </p>
+            </div>
+            <div class="col-md-3">
+              <p class="text-5">
+                Một cơ quan uy tín đã xác thực danh tính chủ nhân Website trước khi cấp chứng chỉ SSL
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-5">
+          <p class="text-3">
+            SSL là viết tắt của từ Secure Sockets Layer. Đây là một tiêu chuẩn an ninh công nghệ toàn cầu tạo ra một liên kết giữa máy chủ web và trình duyệt. Liên kết này đảm bảo tất cả dữ liệu trao đổi giữa máy chủ web và trình duyệt luôn được bảo mật và an toàn.<br><br>SSL đảm bảo rằng tất cả các dữ liệu được truyền giữa các máy chủ web và các trình duyệt được mang tính riêng tư, tách rời. SSL là một chuẩn công nghệ được sử dụng bởi hàng triệu trang web trong việc bảo vệ các giao dịch trực tuyến với khách hàng của họ.
+          </p>
+          <p class="text-4">
+            Quy trình chứng thực Website đã được cấp
+          </p>
+          <img class="screenshot" src="images/ssl-chungthuc.png">
+        </div>
+        <div class="col-md-1"></div>
+      </div>
+    </div>
+  </div>
+  <div class="content-2">
+    <div class="container">
+      <div class="row row-content-2">
+        <div class="col-md-3">
+          <img src="images/icon-shield-ssl.png">
+        </div>
+        <div class="col-md-9">
+          <p class="text-1">
+            Làm thế nào để kiểm tra một SSL là có thực hay không? 
+          </p>
+          <p class="text-2">
+            Khi Website gửi cho trình duyệt một chứng chỉ SSL, trình duyệt sẽ gửi chứng chỉ này đến một máy chủ lưu trữ các chứng chỉ số đã được phê duyệt.<br>
+            Về mặt kỹ thuật, SSL sử dụng mã hóa công khai. Kỹ thuật này giúp cho Website và trình duyệt tự thỏa thuận (bước 4 ở hình trên) một bộ khóa sẽ dùng trong suốt quá trình trao đổi thông tin sau đó. Bộ khóa sẽ thay đổi theo mỗi trong lần giao dịch kế tiếp, một người khác sẽ không thể giải mã ngay cả khi có được dữ liệu của máy chủ lưu trữ chứng chỉ số nói trên.
+          </p>
+          <p class="text-1">
+            Tại sao nên sử dụng SSL?
+          </p>
+          <p class="text-2">
+            Bạn đăng ký domain để sử dụng các dịch vụ website, email v.v... -> luôn có những lỗ hổng bảo mật -> hacker tấn công -> SSL bảo vệ website và khách hàng của bạn.<br>
+            Bảo mật dữ liệu: dữ liệu được mã hóa và chỉ người nhận đích thực mới có thể giải mã.<br>
+            Toàn vẹn dữ liệu: dữ liệu không bị thay đổi bởi tin tặc.<br>
+            Chống chối bỏ: đối tượng thực hiện gửi dữ liệu không thể phủ nhận dữ liệu của mình.
+          </p>
+          <p class="text-1">
+            Lợi ích khi sử dụng SSL?
+          </p>
+          <p class="text-2">
+            Bạn đăng ký domain để sử dụng các dịch vụ website, email v.v… -> luôn có những lỗ hổng bảo mật -> hacker tấn công -> SSL bảo vệ website và khách hàng của bạn.<br>
+            Bảo mật và mã hóa các thông điệp trao đổi giữa trình duyệt và server.<br>
+            Bảo mật các giao dịch giữa khách hàng và doanh nghiệp, các dịch vụ truy nhập hệ thống.<br>
+            Bảo mật webmail và các ứng dụng như Outlook Web Acess, Exchange, và Office Communication Server.<br>
+            Bảo mật các ứng dụng ảo hóa như Citrix Delivery Platform hoặc các ứng dụng điện toán mây.<br>
+            Bảo mật dịch vụ FTP.<br>
+            Bảo mật truy cập Control panel.<br>
+            Bảo mật các dịch vụ truyền dữ liệu trong mạng nội bộ, file sharing, extranet.<br>
+            Bảo mật VPN Access Servers, Citrix Access Gateway.<br>
+            Nâng cao hình ảnh, thương hiệu và uy tín doanh nghiệp.<br>
+            Tạo lợi thế cạnh tranh, tăng niềm tin của khách hàng đối với website, tăng số lượng giao dịch, giá trị giao dịch trực tuyến của khách hàng. Website không được xác thực và bảo mật sẽ luôn ẩn chứa nguy cơ bị xâm nhập dữ liệu, dẫn đến hậu quả khách hàng không tin tưởng sử dụng dịch vụ.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="register-service text-center">
+      <div class="container">
+          <p class="text-register-service">
+              Đăng ký <span>dịch vụ khác</span> kèm theo Cloud VPS của bạn? 
+          </p>
+          <div class="button-register-service">
+              <a href="javascript:void(0)">
+                  <img class="screenshot-2" src="images/btn-register-now.png">
+              </a>
+          </div>
+      </div>
+  </div>
+</div>
+<?php include("footer.php"); ?>
