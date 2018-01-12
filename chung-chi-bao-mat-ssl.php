67714c1fb5fb481e4d7d240accda1e8b1e1f67f7
dungvuit <dungvuit@gmail.com>
2018-01-12 21:39:49 +0700

    File Chung Chi Bao Mat SSL

---
+++ chung-chi-bao-mat-ssl.php
@@ -0,0 +1,231 @@
+<?php include("header.php"); ?>
+<div class="layer-old-2">
+  <div class="slide-chung-chi-bao-mat-ssl text-center">
+      <div class="container">
+          <p class="text-slide-1">CHỨNG CHỈ SSL CHO WEBSITE</p>
+          <p class="text-slide-2">Cung cấp đảm bảo cho khách hàng ngay hôm nay</p>
+      </div>
+  </div>
+  <div class="text-slide-3">
+      <div class="container">
+          <p>
+            Bằng cách sử dụng giấy chứng nhận SSL web, bạn có thể giúp mã hóa thông tin của khách hàng trước khi nó được gửi qua Internet, bảo vệ lợi ích của họ và thúc đẩy sự tự tin của họ trong trang web của bạn.
+          </p>
+      </div>
+  </div>
+  <div class="content-1">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6 text-center col-left-content-1">
+          <div class="row">
+            <div class="col-md-5"></div>
+            <div class="col-md-6">
+              <div class="wrap-content">
+                <p class="text-1">
+                  Thấm nhuần niềm tin vào truy cập của bạn
+                </p>
+                <p class="text-2">
+                  Du khách sẽ không hoàn tất giao dịch của họ hoặc điền vào một mẫu trừ khi họ biết thông tin cá nhân của họ được an toàn. Có Giấy chứng nhận SSL từ BKHOST là cách hợp lý và đơn giản để bảo mật website của bạn và tăng doanh thu.
+                </p>
+                <p class="text-3">
+                  Nhanh chóng SSL @ chỉ
+                </p>
+                <p class="text-4">
+                  250.000
+                </p>
+                <p class="text-5">
+                  - 1 năm -
+                </p>
+                <a href="javascript:void(0)">
+                  Đăng ký ngay
+                </a>
+              </div>
+            </div>
+            <div class="col-md-1"></div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <p class="text-6">Tại sao bạn sẽ cần một SSL từ BKHOST?</p>
+          <p class="text-7">
+            An ninh trang web là tối ưu cho sự thành công kinh doanh trực tuyến của bạn, nhưng nóvẫn là một trong những yếu tố bị bỏ qua nhất khi các doanh nghiệp đang tạo ra sự uy tín trực tuyến trên Internet. Nếu doanh nghiệp của bạn bị thu thập bất kỳ thông tin gì của khách hàng quan trọng, chẳng hạn như thông tin cá nhân, thông tin thẻ tín dụng và nhiều hơn nữa, bạn nên xem xét việc có một chứng chỉ SSL để bảo vệ thông tin của khách hàng mình. Hơn thế nữa, khách hàng chắc chắn sẽ cảm thấy thoải mái hơn khi họ nhìn thấy “Green Lock” hoặc “thanh địa chỉ xanh” trong thanh địa chỉ khi quý khách đến thăm trang web của bạn.<br>
+            Cung cấp cho khách hàng của bạn với một nền địa chỉ mầu xanh và bạn sẽ đón nhận nhiều hơn từ đơn hàng chuyển đổi có uy tín.
+          </p>
+          <p class="text-8">
+            Bảo vệ khi họ truy cập vào bạn:
+          </p>
+          <ul>
+            <li>
+              Đăng ký một tài khoản trên trang web của bạn
+            </li>
+            <li>
+              Cung cấp thông tin cá nhân và thẻ tín dụng
+            </li>
+            <li>
+              Đăng nhập vào cổng thông tin thành viên của bạn
+            </li>
+            <li>
+              Hoàn thành giao dịch
+            </li>
+          </ul>
+          <p class="text-8">
+            Chi tiết kỹ thuật chứng chỉ SSL
+          </p>
+          <ul>
+            <li>
+              Mã hóa các giao dịch trực tuyến của bạn
+            </li>
+            <li>
+              Mã hóa dữ liệu lên đến 256-bit
+            </li>
+            <li>
+              Bảo vệ khỏi lừa đảo và lừa đảo
+            </li>
+            <li>
+              Xác thực
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-2"></div>
+      </div>
+    </div>
+  </div>
+  <div class="content-2 text-center">
+    <div class="container">
+      <p class="text-1">
+        Chứng chỉ SLL như thế nào?
+      </p>
+      <div class="row">
+        <div class="col-md-6">
+          <p class="text-2 text-left">Basic SSL</p>
+          <img src="images/img-chung-chi-ssl-1.png">
+        </div>
+        <div class="col-md-6">
+          <p class="text-2 text-left">Basic with Extended Validation (EV)</p>
+          <img src="images/img-chung-chi-ssl-2.png">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="content-3">
+    <div class="container">
+      <p class="text-1 text-center">
+        Các gói chứng chỉ SSL
+      </p>
+    </div>
+  </div>
+<div class="nav-hosting">
+  <div class="container">
+    <ul class="nav nav-tabs">
+      <li class="active">
+        <a data-toggle="tab" href="#tab1">
+          COMODO SSL
+        </a>
+      </li>
+      <li>
+        <a data-toggle="tab" href="#tab2">
+          GEOTRUS SSL
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
+<div class="tab-hosting">
+  <div class="container">
+    <div class="tab-content">
+      <div id="tab1" class="tab-pane fade in active">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="content-web-hosting">
+              <p class="text-hosting-1">POSITIVESSL</p>
+              <p class="text-hosting-2">2.450.000</p>
+              <strike class="text-hosting-3">3.000.000</strike>
+              <p class="text-hosting-4">- 1 tháng - </p>
+              <div class="discount-code-email">
+                <p class="text-hosting-5">ĐĂNG KÝ</p>
+                <p class="text-hosting-5">TRÊN 6 THÁNG (-15%)</p>
+                <div class="line-discount"><p></p></div>
+                <p class="text-hosting-6">KMSVT715</p>
+              </div>
+              <div class="details-service">
+                <p>Chứng thực DV</p>
+                <p>SAN Không hỗ trợ</p>
+                <p>Bảo hiểm 10.000 USD</p>
+                <p>Thanh trình duyệt mầu xanh Không có</p>
+                <p>Số lượng Domain 01</p>
+                <p>Phí triển khai Miễn phí</p>
+              </div>
+              <a href="#" class="btn-register-hosting">
+                Đăng ký ngay
+              </a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="content-web-hosting">
+              <p class="text-hosting-1">POSITIVESSL</p>
+              <p class="text-hosting-1">MULTI-DOMAIN</p>
+              <p class="text-hosting-2">2.450.000</p>
+              <strike class="text-hosting-3">3.000.000</strike>
+              <p class="text-hosting-4">- 1 tháng - </p>
+              <div class="discount-code-email">
+                <p class="text-hosting-5">ĐĂNG KÝ</p>
+                <p class="text-hosting-5">TRÊN 6 THÁNG (-15%)</p>
+                <div class="line-discount"><p></p></div>
+                <p class="text-hosting-6">KMSVT715</p>
+              </div>
+              <div class="details-service">
+                <p>Chứng thực DV</p>
+                <p>SAN Không hỗ trợ</p>
+                <p>Bảo hiểm 10.000 USD</p>
+                <p>Thanh trình duyệt mầu xanh Không có</p>
+                <p>Số lượng Domain 01</p>
+                <p>Phí triển khai Miễn phí</p>
+              </div>
+              <a href="#" class="btn-register-hosting">
+                Đăng ký ngay
+              </a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="content-web-hosting">
+              <p class="text-hosting-1">EV SSL</p>
+              <p class="text-hosting-2">2.450.000</p>
+              <strike class="text-hosting-3">3.000.000</strike>
+              <p class="text-hosting-4">- 1 tháng - </p>
+              <div class="discount-code-email">
+                <p class="text-hosting-5">ĐĂNG KÝ</p>
+                <p class="text-hosting-5">TRÊN 6 THÁNG (-15%)</p>
+                <div class="line-discount"><p></p></div>
+                <p class="text-hosting-6">KMSVT715</p>
+              </div>
+              <div class="details-service">
+                <p>Chứng thực DV</p>
+                <p>SAN Không hỗ trợ</p>
+                <p>Bảo hiểm 10.000 USD</p>
+                <p>Thanh trình duyệt mầu xanh Không có</p>
+                <p>Số lượng Domain 01</p>
+                <p>Phí triển khai Miễn phí</p>
+              </div>
+              <a href="#" class="btn-register-hosting">
+                Đăng ký ngay
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+  <div class="register-service text-center">
+      <div class="container">
+          <p class="text-register-service">
+              Đăng ký <span>dịch vụ khác</span> kèm theo Cloud VPS của bạn? 
+          </p>
+          <div class="button-register-service">
+              <a href="javascript:void(0)">
+                  <img class="screenshot-2" src="images/btn-register-now.png">
+              </a>
+          </div>
+      </div>
+  </div>
+</div>
+<?php include("footer.php"); ?>
