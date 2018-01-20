104dab94a656966250886bfa1039e042aa30574c
dungvuit <dungvuit@gmail.com>
2018-01-20 12:27:48 +0700

    File email-g-suite

---
+++ email-g-suite.php
@@ -0,0 +1,455 @@
+<?php include("header.php"); ?>
+<div class="layer-old-11">
+    <div class="slide-email-g-suite text-center">
+        <div class="container">
+            <h1 class="text-slide-1">Mail G Suite</h1>
+            <h2 class="text-slide-2">
+              Email doanh nghiệp G Suite là sản phẩm số hàng đầu thể giới dành cho các doanh nghiệp toàn cầu
+            </h2>
+        </div>
+    </div>
+    <div class="content-1 text-center">
+      <div class="container">
+        <div class="text-1">
+          Email doanh nghiệp G Suite là sản phẩm số hàng đầu thể giới dành cho các doanh nghiệp toàn cầu
+          Cung cấp email, lưu trữ, làm việc nhóm, lịch, công tác trực tuyến và nhiều hơn nữa, giải pháp điện toán đám may hàng đầu thế giới đã được ủy quyền tại Việt Nam bởi MailViet
+        </div>
+        <div class="text-2">
+          Đang được giảm phí <b>50%</b> bởi Google. Tham khảo bảng phí toàn cầu <a href="javascript:void(0)">tại đây</a>
+        </div>
+      </div>
+    </div>
+    <div class="content-2 text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="wrap-content">
+              <div class="text-1">Basic</div>
+              <div class="text-2">3$</div>
+              <div class="text-3">
+                <strike>5$</strike>
+              </div>
+              <div class="text-4">một người dùng mỗi tháng</div>
+              <div class="text-4">hoặc 30 đô la/người dùng/năm chưa kể thuế.</div>
+              <div class="text-5">
+                <a href="javascript:void(0)">
+                  bắt đầu dùng thử miễn phí
+                </a>
+              </div>
+              <div class="text-6">Email doanh nghiệp thông qua Gmail</div>
+              <div class="text-6">Hội nghị video và hội nghị thoại</div>
+              <div class="text-6">Lịch được chia sẻ</div>
+              <div class="text-6">Tài liệu, bảng tính và bản trình bày</div>
+              <div class="text-6">Hỗ trợ 24/7 qua điện thoại, email và trực tuyến</div>
+              <div class="text-6">Kiểm soát bảo mật và quản lý</div>
+              <div class="text-6">Bộ nhớ đám mây 30 GB</div>
+              <div class="text-7">
+                <a href="javascript:void(0)">
+                  đăng ký
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="wrap-content">
+              <div class="text-1">Business</div>
+              <div class="text-2">10$</div>
+              <div class="text-4">một người dùng mỗi tháng</div>
+              <div class="text-4">hoặc 30 đô la/người dùng/năm chưa kể thuế.</div>
+              <div class="text-5">
+                <a href="javascript:void(0)">
+                  bắt đầu dùng thử miễn phí
+                </a>
+              </div>
+              <div class="text-6">Email doanh nghiệp thông qua Gmail</div>
+              <div class="text-6">Hội nghị video và hội nghị thoại</div>
+              <div class="text-6">Lịch được chia sẻ</div>
+              <div class="text-6">Tài liệu, bảng tính và bản trình bày</div>
+              <div class="text-6">Hỗ trợ 24/7 qua điện thoại, email và trực tuyến</div>
+              <div class="text-6">Kiểm soát bảo mật và quản lý</div>
+              <div class="text-6">Bộ nhớ đám mây 30 GB</div>
+              <div class="text-7">
+                <a href="javascript:void(0)">
+                  đăng ký
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="wrap-content">
+              <div class="text-1">Enterprise</div>
+              <div class="text-2">25$</div>
+              <div class="text-4">một người dùng mỗi tháng</div>
+              <div class="text-4">hoặc 30 đô la/người dùng/năm chưa kể thuế.</div>
+              <div class="text-5">
+                <a href="javascript:void(0)">
+                  bắt đầu dùng thử miễn phí
+                </a>
+              </div>
+              <div class="text-6">Email doanh nghiệp thông qua Gmail</div>
+              <div class="text-6">Hội nghị video và hội nghị thoại</div>
+              <div class="text-6">Lịch được chia sẻ</div>
+              <div class="text-6">Tài liệu, bảng tính và bản trình bày</div>
+              <div class="text-6">Hỗ trợ 24/7 qua điện thoại, email và trực tuyến</div>
+              <div class="text-6">Kiểm soát bảo mật và quản lý</div>
+              <div class="text-6">Bộ nhớ đám mây 30 GB</div>
+              <div class="text-7">
+                <a href="javascript:void(0)">
+                  đăng ký
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="content-3 text-center">
+      <div class="container">
+        <div class="text-1">
+          Tính năng quan trọng
+        </div>
+      </div>
+    </div>
+    <div class="content-4 text-center">
+      <div class="container">
+        <div class="text-1">
+          Kết nối tiện ích: Tiếp cận đồng nghiệp cho dù họ ở đâu
+        </div>
+        <div class="text-2">
+          <ul>
+            <li class="active">
+              <a data-toggle="tab" href="#gmail">Gmail</a>
+            </li>
+            <li>|</li>
+            <li>
+              <a data-toggle="tab" href="#lich">Lịch</a>
+            </li>
+            <li>|</li>
+            <li>
+              <a data-toggle="tab" href="#google">Google+</a>
+            </li>
+            <li>|</li>
+            <li>
+              <a data-toggle="tab" href="#hangouts">Hangouts Meet</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div id="gmail" class="tab-pane fade in active">
+      <div class="container-fluid">
+        <div class="row text-center">
+          <div class="col-md-1"></div>
+          <div class="col-md-1">
+            <a href="javascript:void(0)">
+              <img class="icon-left" src="images/icon-left-content-gmail.png" alt="Icon Left">
+            </a>
+          </div>
+          <div class="col-md-4"></div>
+          <div class="col-md-3">
+            <div class="text-1 text-left">
+              <span>
+                <img src="images/icon-gmail.png" alt="Icon Gmail">
+                Gmail
+              </span>
+            </div>
+            <div class="text-2 text-left">
+              Nhận email tùy chỉnh (@yourcompany.com) và hơn thế nữa 30GB dung lượng lưu trữ hộp thư đến. Tương thích với Microsoft Outlook và các ứng dụng email khác. Miễn phí quảng cáo, hỗ trợ 24/7 và hơn thế nữa.
+            </div>
+            <div class="text-3">
+              <a href="javascript:void(0)">
+                Bắt đầu dùng thử miễn phí
+              </a>
+            </div>
+          </div>
+          <div class="col-md-1">
+            <a href="javascript:void(0)">
+              <img class="icon-right" src="images/icon-right-content-gmail.png" alt="Icon Right">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="content-4 text-center">
+      <div class="container">
+        <div class="text-1">
+          tạo: Tiếp cận đồng nghiệp cho dù họ ở đâu
+        </div>
+        <div class="text-2">
+          <ul>
+            <li class="active">
+              <a data-toggle="tab" href="#tailieu">Tài liệu</a>
+            </li>
+            <li>|</li>
+            <li>
+              <a data-toggle="tab" href="#trangtinh">Trang tính</a>
+            </li>
+            <li>|</li>
+            <li>
+              <a data-toggle="tab" href="#bieumau">Biểu mẫu</a>
+            </li>
+            <li>|</li>
+            <li>
+              <a data-toggle="tab" href="#sites">Sites</a>
+            </li>
+            <li>|</li>
+            <li>
+              <a data-toggle="tab" href="#keep">Keep</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div id="tailieu" class="tab-pane fade in active">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-md-1">
+            <a href="javascript:void(0)">
+              <img class="icon-left" src="images/icon-left-content-gmail.png" alt="Icon Left">
+            </a>
+          </div>
+          <div class="col-md-4 text-left">
+            <div class="text-1">
+              Nhận xét, trò chuyện và chỉnh sửa trong thời gian thực
+            </div>
+            <div class="text-2">
+              Làm việc trên một tài liệu duy nhất với những thành viên trong nhóm hoặc những người bên ngoài công ty bạn. Xem nội dung chỉnh sửa khi người khác gõ, giao tiếp thông qua tính năng trò chuyện tích hợp và đặt câu hỏi bằng cách bao gồm nhận xét.
+            </div>
+          </div>
+          <div class="col-md-6">
+            <img src="images/img-tailieu.png" alt="Tai Lieu">
+          </div>
+          <div class="col-md-1">
+            <a href="javascript:void(0)">
+              <img class="icon-right" src="images/icon-right-content-gmail.png" alt="Icon Right">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="content-4 text-center">
+      <div class="container">
+        <div class="text-1">
+          Truy cập: Lưu trữ tệp và tìm nội dung bạn cần ngay tức thì
+        </div>
+        <div class="text-2">
+          <ul>
+            <li class="active">
+              <a data-toggle="tab" href="#driver">Drive</a>
+            </li>
+            <li>|</li>
+            <li>
+              <a data-toggle="tab" href="#google-cloud">Google Cloud Search</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div id="driver" class="tab-pane fade in active">
+      <div class="container-fluid">
+        <div class="row text-center">
+          <div class="col-md-1"></div>
+          <div class="col-md-1">
+            <a href="javascript:void(0)">
+              <img class="icon-left" src="images/icon-left-content-gmail.png" alt="Icon Left">
+            </a>
+          </div>
+          <div class="col-md-4"></div>
+          <div class="col-md-3">
+            <div class="text-1 text-left">
+              <span>
+                <img src="images/icon-driver.png" alt="Icon Driver">
+                Driver
+              </span>
+            </div>
+            <div class="text-2 text-left">
+              Dễ dàng lưu trữ, đồng bộ hóa và chia sẻ tệp.
+            </div>
+            <div class="text-2 text-left">
+              Lưu trữ an toàn tất cả dữ liệu của công ty bạn ở một nơi. Truy cập tệp dễ dàng từ máy tính để bàn và thiết bị di động cũng như kiểm soát cách các nhóm chia sẻ tệp.
+            </div>
+            <div class="text-3">
+              <a href="javascript:void(0)">
+                Bắt đầu dùng thử miễn phí
+              </a>
+            </div>
+          </div>
+          <div class="col-md-1">
+            <a href="javascript:void(0)">
+              <img class="icon-right" src="images/icon-right-content-gmail.png" alt="Icon Right">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="content-4 text-center">
+      <div class="container">
+        <div class="text-1">
+          Kiểm soát: Quản lý người dùng, thiết bị và dữ liệu một cách bảo mật và dễ dàng.
+        </div>
+        <div class="text-2">
+          <ul>
+            <li class="active">
+              <a data-toggle="tab" href="#adminstrator">Quản trị viên</a>
+            </li>
+            <li>|</li>
+            <li>
+              <a data-toggle="tab" href="#vault">Vault</a>
+            </li>
+            <li>|</li>
+            <li>
+              <a data-toggle="tab" href="#chinhsachungdung">Chính sách ứng dụng</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div id="adminstrator" class="tab-pane fade in active">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-md-1">
+            <a href="javascript:void(0)">
+              <img class="icon-left" src="images/icon-left-content-gmail.png" alt="Icon Left">
+            </a>
+          </div>
+          <div class="col-md-4 text-left">
+            <div class="text-1">
+              Bảo mật và kiểm soát
+            </div>
+            <div class="text-2">
+              Quản trị tập trung giúp thiết lập và quản lý dễ dàng, nhanh chóng. Thêm và xóa người dùng, thiết lập nhóm và thêm tùy chọn bảo mật như xác minh 2 bước và SSO.
+            </div>
+          </div>
+          <div class="col-md-6">
+            <img src="images/img-adminstrator.png" alt="Adminstrator">
+          </div>
+          <div class="col-md-1">
+            <a href="javascript:void(0)">
+              <img class="icon-right" src="images/icon-right-content-gmail.png" alt="Icon Right">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="content-5 text-center">
+        <div class="container">
+            <h2 class="text-1">
+                LỢI ÍCH CỦA DOANH NGHIỆP
+            </h2>
+            <div class="row row-1">
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-10 col-xs-9 text-right">
+                            <div class="text-2">
+                                Email @yourcompany.com
+                            </div>
+                            <div class="text-3">
+                                Gửi email chuyên nghiệp từ địa chỉ web của doanh nghiệp bạn (you@yourcompany.com) và tạo danh sách gửi thư theo nhóm như sales@yourcompany.com.
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-xs-3">
+                            <img src="images/icon-gmail-1x.png" alt="Icon Gmail 1x" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-2 col-xs-3">
+                            <img src="images/icon-quan-ly-1.png" alt="Icon Quan Ly" />
+                        </div>
+                        <div class="col-md-10 col-xs-9 text-left">
+                            <div class="text-2">
+                                Tất cả bộ nhớ bạn cần
+                            </div>
+                            <div class="text-3">
+                                Phiên bản cơ bản của G Suite có 30 GB dung lượng bộ nhớ trực tuyến mỗi người dùng. Để có bộ nhớ không giới hạn, hãy nâng cấp lên phiên bản Business, Enterprise hoặc Team.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row row-1">
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-10 col-xs-9 text-right">
+                            <div class="text-2">
+                                Hỗ trợ trực tiếp 24/7
+                            </div>
+                            <div class="text-3">
+                                Gọi hoặc gửi email tới Nhóm hỗ trợ Google để nhận trợ giúp từ một nhân viên trực tuyến bất cứ khi nào bạn cần. Bạn cũng có thể tìm câu trả lời nhanh trực tuyến trong trung tâm trợ giúp của chúng tôi.
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-xs-3">
+                            <img src="images/icon-server-1.png" alt="Icon Server" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-2 col-xs-3">
+                            <img src="images/icon-backup-3.png" alt="Icon Backup" />
+                        </div>
+                        <div class="col-md-10 col-xs-9 text-left">
+                            <div class="text-2">
+                                Kiểm soát nâng cao của quản trị viên
+                            </div>
+                            <div class="text-3">
+                                Thêm và xóa người dùng, thiết lập nhóm và thêm các tùy chọn bảo mật như xác minh 2 bước và đăng nhập một lần (SSO), tất cả từ bảng điều khiển tập trung của quản trị viên.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-10 col-xs-9 text-right">
+                            <div class="text-2">
+                                Quản lý thiết bị di động
+                            </div>
+                            <div class="text-3">
+                                Bảo mật dữ liệu của công ty với quản lý thiết bị, tính năng này cho phép bạn dễ dàng định vị thiết bị, yêu cầu mật khẩu và xóa dữ liệu nếu cần.
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-xs-3">
+                            <img src="images/icon-phone-wifi.png" alt="Icon Phone Wifi" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-2 col-xs-3">
+                            <img src="images/icon-resource.png" alt="Icon Resource" />
+                        </div>
+                        <div class="col-md-10 col-xs-9 text-left">
+                            <div class="text-2">
+                                Di chuyển dữ liệu dễ dàng
+                            </div>
+                            <div class="text-3">
+                                Sử dụng các dịch vụ và công cụ di chuyển của chúng tôi để di chuyển dữ liệu quan trọng của tổ chức bạn từ giải pháp lưu trữ hiện tại sang G Suite.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="content-6">
+      <div class="container">
+        <img src="images/img-content-6.png" alt="Img Content">
+      </div>
+    </div>
+    <div class="register-service text-center">
+        <div class="container">
+            <p class="text-register-service">
+                Đăng ký <span>dịch vụ khác</span> kèm theo Cloud VPS của bạn? 
+            </p>
+            <div class="button-register-service">
+                <a href="javascript:void(0)">
+                    <img class="screenshot-2" src="images/btn-register-now.png">
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include("footer.php"); ?>
